1a2c85461f404f088a01b7062b56158f6416bede
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2021-11-08 17:06:56 +0100

    Small changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import WindiCSS from 'vite-plugin-windicss';
+//import htmlMinifier from 'rollup-plugin-html-minifier';
+import sri from 'rollup-plugin-sri';
 import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 
 export default defineConfig({
@@ -8,10 +10,44 @@ export default defineConfig({
 		typescriptPaths({ preserveExtensions: true, absolute: false }),
 		solidPlugin(),
 		WindiCSS(),
+		/*htmlMinifier({
+			filter: (file) => {
+				const retVal =
+					typeof file === 'string'
+						? file.endsWith('.html')
+						: (console.warn('Unknown file type', file) as unknown as boolean);
+				console.log(
+					`\n${retVal ? '' : "Didn't "}Minif${retVal ? 'ied' : 'y'}`,
+					file,
+				);
+				return retVal;
+			},
+			options: {
+				collapseWhitespace: true,
+				removeComments: true,
+				decodeEntities: true,
+				minifyCSS: true,
+				minifyJS: true,
+				removeEmptyAttributes: true,
+				processConditionalComments: true,
+			},
+		}),*/
+		{
+			enforce: 'post',
+			...sri({
+				publicPath: '/',
+				algorithms: ['sha1', 'md5', 'sha512', 'sha256'],
+			}),
+		},
 	],
 	server: {
 		watch: {
-			ignored: ['**/coverage/**', '**/dist/**'],
+			ignored: [
+				'**/coverage/**',
+				'**/dist/**',
+				'**/.vscode/**',
+				'**/.github/**',
+			],
 		},
 	},
 	build: {
